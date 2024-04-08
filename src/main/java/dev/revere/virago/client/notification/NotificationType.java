c850d6cc025f3a111e91de8962701ec99fa6ce5e
ZionRank1 <alexpal5715@gmail.com>
2024-04-08 07:52:37 +0100

    icons

--- src/main/java/dev/revere/virago/client/notification/NotificationType.java
+++ src/main/java/dev/revere/virago/client/notification/NotificationType.java
@@ -8,9 +8,9 @@ import java.awt.*;
 @Getter
 @AllArgsConstructor
 public enum NotificationType {
-    INFO(new Color(255, 255, 255, 195), "j"),
-    YES(new Color(56, 182, 42, 195), "j"),
-    NO(new Color(182, 42, 42, 195), "j"),
+    INFO(new Color(255, 255, 255, 195), "t"),
+    YES(new Color(56, 182, 42, 195), "D"),
+    NO(new Color(182, 42, 42, 195), "C"),
     WARNING(new Color(194, 161, 7, 194), "j"),
     ERROR(new Color(168, 20, 20, 194), "j");
 
