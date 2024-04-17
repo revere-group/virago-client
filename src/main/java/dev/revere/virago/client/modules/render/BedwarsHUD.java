39eee0a7232654f4e8dd0fc4be546f655792d078
ZionRank1 <alexpal5715@gmail.com>
2024-04-17 20:50:50 +0100

    bedwars hud and debug prints removed from sessioninfo

--- src/main/java/dev/revere/virago/client/modules/render/BedwarsHUD.java
+++ src/main/java/dev/revere/virago/client/modules/render/BedwarsHUD.java
@@ -11,7 +11,6 @@ import dev.revere.virago.api.module.ModuleData;
 import dev.revere.virago.api.setting.Setting;
 import dev.revere.virago.client.events.packet.PacketEvent;
 import dev.revere.virago.client.events.render.Render2DEvent;
-import dev.revere.virago.client.events.player.JoinEvent;
 import dev.revere.virago.client.events.player.LeaveEvent;
 import dev.revere.virago.client.events.render.ShaderEvent;
 import dev.revere.virago.client.services.DraggableService;
@@ -21,14 +20,13 @@ import dev.revere.virago.util.render.RoundedUtils;
 import net.minecraft.network.play.server.S02PacketChat;
 import net.minecraft.network.play.server.S45PacketTitle;
 import net.minecraft.util.StringUtils;
-import org.apache.commons.lang3.time.DurationFormatUtils;
 
 import java.awt.*;
+import java.util.ArrayList;
 
-@ModuleData(name = "Session Info", description = "Modify the scoreboard aesthetics.", type = EnumModuleType.RENDER)
-public class SessionInfo extends AbstractModule {
+@ModuleData(name = "Bedwars Hud", description = "Add a bedwars HUD", type = EnumModuleType.RENDER)
+public class BedwarsHUD extends AbstractModule {
 
-    private final Setting<Boolean> shadow = new Setting<>("Text Shadow", true);
     public Setting<FontType> fontType = new Setting<>("Font", FontType.PRODUCT_SANS).describedBy("The font type to use.");
     private final Setting<Integer> opacity = new Setting<>("Opacity", 180)
             .minimum(0)
@@ -36,12 +34,14 @@ public class SessionInfo extends AbstractModule {
             .incrementation(1)
             .describedBy("The opacity for background");
 
-    private Draggable draggable = Virago.getInstance().getServiceManager().getService(DraggableService.class).addDraggable(new Draggable(this, "Session Info", 200, 200));
+    private Draggable draggable = Virago.getInstance().getServiceManager().getService(DraggableService.class).addDraggable(new Draggable(this, "Bedwars", 200, 200));
 
     FontRenderer fontRenderer;
-    private int kills = 0;
-    private int wins = 0;
-    private long sessionTime;
+
+    private boolean sharp = false;
+    private int prot = 0;
+    private ArrayList<String> traps = new ArrayList<>();
+
 
     @EventHandler
     public Listener<Render2DEvent> onRender2D = event -> {
@@ -54,16 +54,16 @@ public class SessionInfo extends AbstractModule {
         RenderUtils.renderGradientRect((int) draggable.getX(), (int) draggable.getY() + 14, (int) (115 + draggable.getX()), (int) (draggable.getY() + 15), 5.0, 2000L, 2L, RenderUtils.Direction.RIGHT);
         //RoundedUtils.round(draggable.getX(), draggable.getY() - 1, 113, 52, 4, new Color(20,20,20, 200));
 
-        fontRenderer.drawStringWithShadow("Session Stats", draggable.getX() + 25, draggable.getY() + 2, -1);
+        fontRenderer.drawStringWithShadow("Bedwars", draggable.getX() + 25, draggable.getY() + 2, -1);
 
         font.getIcon10().drawString("b", draggable.getX() + 2, draggable.getY() + 22, -1);
-        fontRenderer.drawStringWithShadow("Duration: " + getSessionTime(), draggable.getX() + 8, draggable.getY() + 18, -1);
+        fontRenderer.drawStringWithShadow("Trap: " + getTrap(), draggable.getX() + 8, draggable.getY() + 18, -1);
 
         font.getIcon10().drawString("a", draggable.getX() + 2, draggable.getY() + 31, -1);
-        fontRenderer.drawStringWithShadow("Kills: " + this.kills, draggable.getX() + 8, draggable.getY() + 27, -1);
+        fontRenderer.drawStringWithShadow("Sharpness: " + this.sharp, draggable.getX() + 8, draggable.getY() + 27, -1);
 
         font.getIcon10().drawString("v", draggable.getX() + 2, draggable.getY() + 40, -1);
-        fontRenderer.drawStringWithShadow("Wins: " + this.wins, draggable.getX() + 8, draggable.getY() + 36, -1);
+        fontRenderer.drawStringWithShadow("Protection: " + this.prot, draggable.getX() + 8, draggable.getY() + 36, -1);
 
         draggable.setWidth(100);
         draggable.setHeight(50);
@@ -74,16 +74,16 @@ public class SessionInfo extends AbstractModule {
         FontService font = Virago.getInstance().getServiceManager().getService(FontService.class);
         getFont(font);
 
-        fontRenderer.drawStringWithShadow("Session Stats", draggable.getX() + 25, draggable.getY() + 2, -1);
+        fontRenderer.drawStringWithShadow("Bedwars", draggable.getX() + 25, draggable.getY() + 2, -1);
 
         font.getIcon10().drawString("b", draggable.getX() + 2, draggable.getY() + 22, -1);
-        fontRenderer.drawStringWithShadow("Duration: " + getSessionTime(), draggable.getX() + 8, draggable.getY() + 18, -1);
+        fontRenderer.drawStringWithShadow("Trap: " + getTrap(), draggable.getX() + 8, draggable.getY() + 18, -1);
 
         font.getIcon10().drawString("a", draggable.getX() + 2, draggable.getY() + 31, -1);
-        fontRenderer.drawStringWithShadow("Kills: " + this.kills, draggable.getX() + 8, draggable.getY() + 27, -1);
+        fontRenderer.drawStringWithShadow("Sharpness: " + this.sharp, draggable.getX() + 8, draggable.getY() + 27, -1);
 
         font.getIcon10().drawString("v", draggable.getX() + 2, draggable.getY() + 40, -1);
-        fontRenderer.drawStringWithShadow("Wins: " + this.wins, draggable.getX() + 8, draggable.getY() + 36, -1);
+        fontRenderer.drawStringWithShadow("Protection: " + this.prot, draggable.getX() + 8, draggable.getY() + 36, -1);
     };
 
     @EventHandler
@@ -93,27 +93,46 @@ public class SessionInfo extends AbstractModule {
             if (s45.getMessage() == null) return;
 
             if (StringUtils.stripControlCodes(s45.getMessage().getUnformattedText()).equals("VICTORY!")) {
-                this.wins++;
+                this.prot = 0;
+                this.sharp = false;
+                this.traps.clear();
             }
+
         } else if (event.getPacket() instanceof S02PacketChat) {
             S02PacketChat s02 = event.getPacket();
             String message = s02.getChatComponent().getUnformattedText();
 
-            if (message.contains("killed by " + mc.thePlayer.getName()) || message.contains("killed by " + Virago.getInstance().getViragoUser().getUsername())) {
-                this.kills++;
+            if(message.contains("Sharpened Swords")) {
+                sharp = true;
+            } else if(message.contains("Reinforced Armor")) {
+                prot++;
+            } else if(message.contains("purchased Counter-Offensive")) {
+                traps.add("Counter-Offensive");
+            } else if(message.contains("purchased Alarm")) {
+                traps.add("Alarm");
+            } else if(message.contains("purchased Miner Fatigue")) {
+                traps.add("Miner Fatigue");
+            } else if(message.contains("purchased It's a trap!")) {
+                traps.add("Normal");
+            } else if(message.contains("Miner Fatigue Trap was set off!")) {
+                traps.remove(0);
+            } else if(message.contains("Counter-Offensive Trap was set off!")) {
+                traps.remove(0);
+            } else if(message.contains("Alarm Trap was set off!")) {
+                traps.remove(0);
+            } else if(message.contains("It's a trap! was set off!")) {
+                traps.remove(0);
             }
         }
     };
 
     @EventHandler
-    public final Listener<JoinEvent> onJoin = event -> {
-        this.sessionTime = System.currentTimeMillis();
+    public final Listener<LeaveEvent> onLeave = event -> {
+        this.prot = 0;
+        this.sharp = false;
+        this.traps.clear();
     };
 
-    @EventHandler
-    public final Listener<LeaveEvent> onLeave = event -> {
-        this.sessionTime = 0;
-    };
 
     /**
      * Gets the font
@@ -137,12 +156,14 @@ public class SessionInfo extends AbstractModule {
         }
     }
 
-    private String getSessionTime() {
-        if(this.sessionTime == 0)
-            return "Invalid Time";
+    private String getTrap() {
+        if(traps.isEmpty()) {
+            return "None";
+        }
 
-        return DurationFormatUtils.formatDuration(System.currentTimeMillis() - sessionTime, "H'h,' m'm,' s's'");
+        return traps.get(0);
     }
+
 
     public enum FontType {
         PRODUCT_SANS,
@@ -153,13 +174,11 @@ public class SessionInfo extends AbstractModule {
 
     @Override
     public void onEnable() {
-        this.sessionTime = System.currentTimeMillis();
         super.onEnable();
     }
 
     @Override
     public void onDisable() {
-        this.sessionTime = 0;
         super.onDisable();
     }
 }
