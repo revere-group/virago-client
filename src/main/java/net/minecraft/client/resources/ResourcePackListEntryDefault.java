12215cbab48925a9f9769900721e5a9d4b5483b2
refactoring <refactoringg@duck.com>
2024-04-30 15:16:45 +0530

    fix: resource packs

--- src/main/java/net/minecraft/client/resources/ResourcePackListEntryDefault.java
+++ src/main/java/net/minecraft/client/resources/ResourcePackListEntryDefault.java
@@ -27,7 +27,7 @@ public class ResourcePackListEntryDefault extends ResourcePackListEntry
         {
             dynamictexture = new DynamicTexture(this.field_148320_d.getPackImage());
         }
-        catch (IOException var4)
+        catch (Exception var4)
         {
             dynamictexture = TextureUtil.missingTexture;
         }
