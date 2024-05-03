696205795af37b6adedecf660ec094de3629b3e8
devuxious <remig06@hotmail.com>
2024-05-03 18:55:29 +0200

    fix: remove discord-game-sdk from shadow task

--- build.gradle.kts
+++ build.gradle.kts
@@ -82,7 +82,7 @@ tasks.register<JavaExec>("run") {
 tasks.shadowJar {
     dependencies {
         include(dependency("com.github.Vatuu:discord-rpc:1.6.2"))
-        include(dependency("com.github.JnCrMx:discord-game-sdk4j:0.5.5"))
+        //include(dependency("com.github.JnCrMx:discord-game-sdk4j:0.5.5"))
         //include(dependency("org.projectlombok:lombok"))
         include(dependency("org.reflections:reflections:0.10.2"))
         include(dependency("org.json:json:20230227"))
