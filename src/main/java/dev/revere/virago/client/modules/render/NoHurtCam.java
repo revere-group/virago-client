9f2c1030ac3faee011cfd525fea4cdb11225c8a7
ziuee <remig06@hotmail.com>
2024-05-07 14:11:59 +0200

    fix: get rid of code from nohurtcam cuz the developer adding this is autistic

--- src/main/java/dev/revere/virago/client/modules/render/NoHurtCam.java
+++ src/main/java/dev/revere/virago/client/modules/render/NoHurtCam.java
@@ -1,28 +1,11 @@
 package dev.revere.virago.client.modules.render;
 
-import dev.revere.virago.api.event.handler.EventHandler;
-import dev.revere.virago.api.event.handler.Listener;
 import dev.revere.virago.api.module.AbstractModule;
 import dev.revere.virago.api.module.EnumModuleType;
 import dev.revere.virago.api.module.ModuleData;
-import dev.revere.virago.client.events.game.TickEvent;
-import dev.revere.virago.client.events.packet.PacketEvent;
-import dev.revere.virago.client.events.player.PreMotionEvent;
-import net.minecraft.client.Minecraft;
-import net.minecraft.network.play.server.S42PacketCombatEvent;
-import org.lwjgl.Sys;
-
 
 @ModuleData(name = "NoHurtCam", displayName = "No Hurt Cam", description = "Disable hurt camera", type = EnumModuleType.RENDER)
-public class NoHurtCam extends AbstractModule { // TODO: No clue if this works.
-
-    @EventHandler
-    private final Listener<PreMotionEvent> preMotionEventListener = event -> {
-        if (this.isEnabled() && Minecraft.getMinecraft().thePlayer != null) {
-            Minecraft.getMinecraft().thePlayer.hurtTime = 0;
-            Minecraft.getMinecraft().thePlayer.hurtResistantTime = 0;
-        }
-    };
+public class NoHurtCam extends AbstractModule {
 
     @Override
     public void onEnable() {
